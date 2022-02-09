9f53a5e6f94413b53f096183cff07f6da7273efb
Nisflei <ncgaloni@hotmail.com>
2022-02-09 20:41:45 -0300

    Aula3

---
+++ Aula3/src/implemantacao/ExecutarProduto.java
@@ -0,0 +1,36 @@
+package implemantacao;
+
+import java.util.Scanner;
+
+import beans.Produto;
+
+public class ExecutarProduto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Produto p1 = new Produto();
+		
+		p1.setNome("Notebook 14");
+		p1.setValor(1000.0005f);
+		System.out.println(p1.detalhe());
+		System.out.println(p1.getMarca());
+		
+		Produto p2 = new Produto("Impressora HP", 100.0f);
+		
+		System.out.println(p2.detalhe());
+		System.out.println(p2.getMarca());
+		
+		Produto p3 = new Produto("Sansung TV","LG",5000.50f,true);
+		System.out.println("p3-"+ p3.detalhe());
+		
+		System.out.println(p3.getMarca());
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		p2.setMarca(leitor.next());
+		System.out.println("p2-marca:" + p2.getMarca());
+		
+		
+	}
+
+}
