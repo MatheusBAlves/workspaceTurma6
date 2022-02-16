6e88e2b8eae705b9588195408ea9bcd127b578b0
Nisflei <ncgaloni@hotmail.com>
2022-02-16 20:37:38 -0300

    Aula6

--- Consultorio/src/br/com/fiap/consultorio/beans/Gerente.java
+++ Consultorio/src/br/com/fiap/consultorio/beans/Gerente.java
@@ -1,9 +1,18 @@
 package br.com.fiap.consultorio.beans;
 
+import java.util.Date;
+
 public class Gerente extends Funcionario {
 	private String deptResponsavel;
 
+	public Gerente () {}
 	
+	public Gerente(String nome, String cpf, String fone, Date dtadmissao, Date dtdemissao, double salario,
+			String deptResponsavel) {
+		super(nome, cpf, fone, dtadmissao, dtdemissao, salario);
+		this.deptResponsavel = deptResponsavel;
+	}
+
 	public String getDeptResponsavel() {
 		return deptResponsavel;
 	}
--- Consultorio/src/br/com/fiap/consultorio/implementacao/ExecutarAgenda.java
+++ Consultorio/src/br/com/fiap/consultorio/implementacao/ExecutarAgenda.java
@@ -6,9 +6,9 @@ import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 
 import br.com.fiap.consultorio.beans.Funcionario;
+import br.com.fiap.consultorio.beans.Gerente;
 import br.com.fiap.consultorio.beans.Medico;
 import br.com.fiap.consultorio.beans.Paciente;
-import br.com.fiap.consultorio.beans.Pessoa;
 
 public class ExecutarAgenda {
 
@@ -22,9 +22,13 @@ public class ExecutarAgenda {
 		Funcionario fun = new Funcionario("Ana Maria","Cpf12346","98989898",formatarData.parse("03/02/2022"), null, 500);
 		
 		Medico medico = new Medico("Dr. Fausto","CPF123789789","9999999",formatarData.parse("16/02/2022"),null,5000,"Cardiologista","101010");
-		
+
+		Gerente g = new Gerente("BONINHO", "Cpf1234679", "9999999", null, null, 10000, "Finanças");
+				
+				
 		JOptionPane.showMessageDialog(null, "Funcionario: " + fun.getNome() + "\n\tSalario: "+ fun.getSalario() + "\n\tParticipação: " + fun.calcularParticipacaoNosLucros());
 		JOptionPane.showMessageDialog(null, "Medico: " + medico.getNome() + " \n\tSalario: " + medico.getSalario() + "\n\tParticipação: " + medico.calcularParticipacaoNosLucros() );
+		JOptionPane.showMessageDialog(null, "Gerente: " + g.getNome() + " \n\tSalario: " + g.getSalario() + "\n\tParticipação: " + g.calcularParticipacaoNosLucros() );
 
 		
 	}
